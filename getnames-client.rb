c2beacb5b3f14fc17e9ca6beff9d9905ef61375a
Bryan Venable <github@spif.com>
2016-11-07 17:57:28 -0600

    add client

---
+++ getnames-client.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'uri'
+require 'json'
+webgetnames = ENV['WEBGETNAMES']
+if ARGV[0].nil?
+ generator = 'superheronameorg'
+else
+ generator = ARGV[0]
+ if generator == 'silly'
+  generator = 'sillyhero'
+ end
+ if generator == 'quickname'
+  type = ARGV[1]
+ end
+ if generator == 'female'
+  generator = 'quickname'
+  type = 'FNAME'
+ end
+ if generator == 'male'
+  generator = 'quickname'
+  type = "MNAME"
+ end
+end
+if type.nil?
+ uri = URI.parse(webgetnames + generator)
+else
+ uri = URI.parse(webgetnames + generator + "/" + type)
+end
+request = Net::HTTP::Get.new(uri.request_uri)
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+response = http.request(request)
+names = JSON.parse(response.body)
+puts names
